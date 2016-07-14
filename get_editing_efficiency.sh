7bb632157b6177daeb62eec50cb39ef2708627d0
Benoit Castandet <bc467@cornell.edu>
2016-07-14 16:06:26 -0400

    update

--- get_editing_efficiency.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Collect variables from script
-bam_name=$1
-fasta=$2
-
-# create a pileup file containing the SNP for the genome of interest using samtools mpileup, -I to not consider indels
-samtools mpileup -I -f $fasta $bam_name > editing.pileup 
